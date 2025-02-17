5e3907f80ae3dec0e61c8f56b1719282ce5f0b06
yanari8 <yanari8co@gmail.com>
2025-02-17 11:45:20 +0900

    課題「クエリビルダ」

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,10 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
     public function index() {
-        return view("posts.index"); 
+        // postsテーブルから全てのデータを取得→変数　$posts に代入
+        $posts = DB::table('posts')->get();
+
+        //　変数　$postsをposts/index/blade/php ファイルに渡す
+        return view("posts.index", compact('posts'));
     }
+
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,8 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel基礎課題</title>
+    <style>
+        /* 投稿を囲む */
+        .post{
+            border: 1px solid #dddddd;
+            padding: 10px;
+            margin-bottom: 15px;
+        }
+
+        /* タイトル 太字*/
+        .post-title{
+            font-weight: bold;
+            font-size: 18px;
+            padding-bottom: 2px;
+            margin-bottom: 2px;
+            border-bottom: 1px dotted #cec4f6;
+        }
+
+        /* 本文 */
+        .post-content{
+            font-size: 16px;
+        }
+
+    </style>
 </head>
 <body>
     <h1>投稿一覧</h1>
+
+
+        @foreach ($posts as $post)
+            <div class="post">
+                <div class="post-title">{{ $post->title}}</div>
+                <div class="post-content">{{ $post->content}}</div>
+            </div>
+        @endforeach
+
 </body>
 </html>
